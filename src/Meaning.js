94acfc7bfb7f1aea3b3ff463bcff571947996ad1
Maria Medeiros <maitevelori@hotmail.com>
2022-07-03 07:43:42 -0700

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
@@ -9,9 +9,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.defininton}
-
+              <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
