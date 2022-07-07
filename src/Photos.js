8ed9c150073f6437a167564060a1b47e3ef00ba3
Maria Medeiros <maitevelori@hotmail.com>
2022-07-07 16:40:27 -0700

    added alt in my img

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="photos"
+                  />
                 </a>
               </div>
             );
